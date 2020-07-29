4387967582386e0cec43b8b8709da2c09add1316
Bryan <48531026+bweiss0503@users.noreply.github.com>
2020-07-29 16:30:35 -0500

    Update convert-json.py

--- convert-json.py
+++ convert-json.py
@@ -6,11 +6,11 @@ import tkinter as tk
 
 root = tk.Tk()
 
-canvas = tk.Canvas(root, width = 400, height = 300)
-canvas.pack()
-
 label = tk.Label(root, text = "Enter JSON File Location")
 label.pack()
+
+canvas = tk.Canvas(root, width = 400, height = 300)
+canvas.pack()
 
 entry = tk.Entry(root) 
 canvas.create_window(200, 140, window=entry)
@@ -23,7 +23,10 @@ def getResults ():
 		print("Invalid File Path")
 	else:
 		f = open(filename, 'r')
-		res = open('results.csv', 'w+')
+		i=0
+		while os.path.exists("results%s.csv" % i):
+			i += 1
+		res = open('results%s.csv' % i, 'w+')
 		emails = re.findall(r'[\w\.-]+@[\w\.-]+', f.read())
 		for email in emails:
 			res.write("%s\n" % email)
