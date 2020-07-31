5f97ac72afe230b7947bd1ad37bee52367ebdbf6
Bryan <48531026+bweiss0503@users.noreply.github.com>
2020-07-31 10:49:05 -0500

    Update convert-json.py

--- convert-json.py
+++ convert-json.py
@@ -14,26 +14,33 @@ canvas.pack()
 
 entry = tk.Entry(root) 
 canvas.create_window(200, 140, window=entry)
+#sets up program tkinter window with text entry
 
 def getResults ():
 	filename = entry.get()
-
 	isFile = os.path.isfile(filename)
+
 	if not (isFile):
 		print("Invalid File Path")
+		return
+	#ends getResults if path is bad
 	else:
 		f = open(filename, 'r')
 		i=0
 		while os.path.exists("results%s.csv" % i):
 			i += 1
 		res = open('results%s.csv' % i, 'w+')
+		#adds number to result.csv
 		emails = re.findall(r'[\w\.-]+@[\w\.-]+', f.read())
+		#finds emails within json file
 		for email in emails:
 			res.write("%s\n" % email)
+		#writes to csv, emails on individual lines
 		f.close()
 	res.close()
 
 button = tk.Button(text='Enter', command=getResults)
 canvas.create_window(200, 180, window=button)
+#creates button that runs getResults function
 
 root.mainloop()
